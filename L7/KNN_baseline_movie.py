281bb2caf597a3ca5feefc5badd4402623012e46
wangyu33 <1134711093@qq.com>
2020-04-12 16:36:56 +0800

    提交hw

---
+++ L7/KNN_baseline_movie.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File  : KNN_baseline_movie.py
+# Author: WangYu
+# Date  : 2020-04-12
+
+from surprise import KNNWithMeans
+from surprise import Dataset, Reader
+from surprise import KNNBaseline
+from surprise.model_selection import KFold
+from surprise import accuracy
+
+# 数据读取
+reader = Reader(line_format='user item rating timestamp', sep=',', skip_lines=1)
+data = Dataset.load_from_file('./ratings.csv', reader=reader)
+
+KF = KFold(n_splits=3)
+algo = KNNWithMeans(k=50, sim_options={'user_based': False, 'verbose': 'True'})
+
+for train, test in KF.split(data):
+    algo.fit(train)
+    predictions = algo.test(test)
+    accuracy.rmse(predictions, verbose=True)
+    accuracy.mae(predictions, verbose=True)
+
+
+
+
